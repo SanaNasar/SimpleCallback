55ec8b6608406a03d90cbd7466ae1328b5404512
SanaNasar <sanasehereen.cse@gmail.com>
2015-01-05 19:33:48 -0800

    Adding step 1

---
+++ script.js
@@ -0,0 +1,10 @@
+//Step 1: 
+
+//Create a fuction add :
+
+function add(first, second) { // function add takes in two arguments
+  console.log(first+second);
+}
+
+add(2,3);
+//Result will return 5 in the console
