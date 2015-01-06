43b9559d47d95675a412870581b7262f3487141f
SanaNasar <sanasehereen.cse@gmail.com>
2015-01-05 19:39:25 -0800

    Adding a callback argument

--- script.js
+++ script.js
@@ -8,3 +8,19 @@ function add(first, second) { // function add takes in two arguments
 
 add(2,3);
 //Result will return 5 in the console
+
+
+//Step 2: 
+
+//Add a callback argument
+
+function add(first, second, callback) {
+  console.log(first+second);
+  callback();
+}
+
+add(2,3, function(){
+  console.log('Bam!');
+});
+//Result will return 5 and Bam!
+
